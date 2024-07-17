8777635a083086bf011765b57d062b2e24f1515f
crochethk <70408499+crochethk@users.noreply.github.com>
2024-07-17 21:58:39 +0200

    epg: Add unsolvable config combination example to `main.py`

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import eight_puzzle_heuristics as ep_heurs
 from search_strategy import BreadthFirstSearch, DepthFirstSearch, AStarSearch
 
 
-def main():
+def assignment_example_solution():
     init_state = EightPuzzleBoard([[2, 8, 3], [1, 6, 4], [7, None, 5]])
     goal_state = EightPuzzleBoard([[1, 2, 3], [8, None, 4], [7, 6, 5]])
     # ===================== Breadth First=======================================
@@ -68,7 +68,30 @@ def main():
     # >>> Expansions to finish: 1724
     # >>> [up, up, left, down, right]
 
+
+def main():
+    #assignment_example_solution()
+
     # ==========================================================================
+
+    # An UNSOLVABLE configuration combination
+    init_state = EightPuzzleBoard([[4, 7, 8], [5, None, 3], [6, 1, 2]])
+    goal_state = EightPuzzleBoard([[1, 2, 3], [8, None, 4], [7, 6, 5]])
+
+    # ===================== Breadth First=======================================
+    print("\n~~~ A* - Heuristic 1: 'cumulative_distance' ~~~")
+    puzzle = EightPuzzle(init_state,
+                         goal_state,
+                         AStarSearch(goal_state, ep_heurs.cumulative_distance))
+    print(puzzle)
+
+    # print("--- Exhaustive search ---")
+    print(puzzle.solve())
+    # >>> None
+
+    print("--- Lazy search ---")
+    print(puzzle.solve(False))
+    # >>> None
 
 
 if __name__ == "__main__":
