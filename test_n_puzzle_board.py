2cc438211288ae8e17c34bce285dc14576eebc5a
crochethk <70408499+crochethk@users.noreply.github.com>
2024-07-19 20:55:26 +0200

    Add unittests for 15 puzzle board

--- test_n_puzzle_board.py
+++ test_n_puzzle_board.py
@@ -1,10 +1,11 @@
 import unittest
 
 from eight_puzzle import EightPuzzleBoard as EPB
+# from n_puzzle import EightPuzzleBoard as NPB
 from eight_puzzle import MoveDirection as mv
 
 
-class TestEPBoard(unittest.TestCase):
+class Test8PuzzleBoard(unittest.TestCase):
     def test_next_states_1(self):
         s_init = [[2, 8, 3], [1, 6, 4], [7, None, 5]]
         history = []
@@ -56,6 +57,104 @@ class TestEPBoard(unittest.TestCase):
             ([mv.RIGHT, mv.DOWN], EPB([[2, 8, 3], [1, None, 4], [7, 6, 5]])),
             ([mv.RIGHT, mv.RIGHT], EPB([[2, 3, None], [1, 8, 4], [7, 6, 5]])),
             ([mv.RIGHT, mv.LEFT], EPB([[None, 2, 3], [1, 8, 4], [7, 6, 5]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+
+class Test15PuzzleBoard(unittest.TestCase):
+    def test_next_states_1(self): # "inner row"
+        s_init = [[2, 8, 3, 9], [1, 6, 4, 10], [7, None, 5, 11], [12, 13, 14, 15]]
+        history = []
+        expect_next_states = [
+            ([mv.DOWN], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [7, 13, 5, 11], [12, None, 14, 15]])),
+            ([mv.UP], EPB([[2, 8, 3, 9], [1, None, 4, 10], [7, 6, 5, 11], [12, 13, 14, 15]])),
+            ([mv.RIGHT], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [7, 5, None, 11], [12, 13, 14, 15]])),
+            ([mv.LEFT], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [None, 7, 5, 11], [12, 13, 14, 15]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_2(self): # "inner row"
+        s_init = [[2, 8, 3, 9], [1, 6, 4, 10], [None, 7, 5, 11], [12, 13, 14, 15]]
+        history = [mv.DOWN]
+        expect_next_states = [
+            ([mv.DOWN, mv.DOWN], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [12, 7, 5, 11], [None, 13, 14, 15]])),
+            ([mv.DOWN, mv.UP], EPB([[2, 8, 3, 9], [None, 6, 4, 10], [1, 7, 5, 11], [12, 13, 14, 15]])),
+            ([mv.DOWN, mv.RIGHT], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [7, None, 5, 11], [12, 13, 14, 15]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_3(self): # "inner row"
+        s_init = [[2, 8, 3, 9], [1, 4, 10, None], [7, 6, 5, 11], [12, 13, 14, 15]]
+        history = [mv.UP]
+        expect_next_states = [
+            ([mv.UP, mv.DOWN], EPB([[2, 8, 3, 9], [1, 4, 10, 11], [7, 6, 5, None], [12, 13, 14, 15]])),
+            ([mv.UP, mv.UP], EPB([[2, 8, 3, None], [1, 4, 10, 9], [7, 6, 5, 11], [12, 13, 14, 15]])),
+            ([mv.UP, mv.LEFT], EPB([[2, 8, 3, 9], [1, 4, None, 10], [7, 6, 5, 11], [12, 13, 14, 15]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_4(self): # "bottom row"
+        s_init = [[2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15], [7, None, 5, 11]]
+        history = [mv.LEFT]
+        expect_next_states = [
+            ([mv.LEFT, mv.UP], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [12, None, 14, 15], [7, 13, 5, 11]])),
+            ([mv.LEFT, mv.RIGHT], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15], [7, 5, None, 11]])),
+            ([mv.LEFT, mv.LEFT], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15], [None, 7, 5, 11]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_5(self): # "bottom row"
+        s_init = [[2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15], [None, 7, 5, 11]]
+        history = [mv.RIGHT]
+        expect_next_states = [
+            ([mv.RIGHT, mv.UP], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [None, 13, 14, 15], [12, 7, 5, 11]])),
+            ([mv.RIGHT, mv.RIGHT], EPB([[2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15], [7, None, 5, 11]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_6(self): # "bottom row"
+        s_init = [[2, 8, 3, 9], [7, 6, 5, 11], [12, 13, 14, 15], [1, 4, 10, None]]
+        history = []
+        expect_next_states = [
+            ([mv.UP], EPB([[2, 8, 3, 9], [7, 6, 5, 11], [12, 13, 14, None], [1, 4, 10, 15]])),
+            ([mv.LEFT], EPB([[2, 8, 3, 9], [7, 6, 5, 11], [12, 13, 14, 15], [1, 4, None, 10]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_7(self): # "top row"
+        s_init = [[7, None, 5, 11], [2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15],]
+        history = []
+        expect_next_states = [
+            ([mv.DOWN], EPB([[7, 8, 5, 11], [2, None, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15]])),
+            ([mv.RIGHT], EPB([[7, 5, None, 11], [2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15]])),
+            ([mv.LEFT], EPB([[None, 7, 5, 11], [2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15]])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_8(self): # "top row"
+        s_init = [[None, 7, 5, 11], [2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15],]
+        history = []
+        expect_next_states = [
+            ([mv.DOWN], EPB([[2, 7, 5, 11], [None, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15],])),
+            ([mv.RIGHT], EPB([[7, None, 5, 11], [2, 8, 3, 9], [1, 6, 4, 10], [12, 13, 14, 15],])),
+        ]
+        next_states = EPB(s_init).next_valid_board_states(history)
+        self.assertEqual(next_states, expect_next_states)
+
+    def test_next_states_9(self): # "top row"
+        s_init = [[1, 4, 10, None], [2, 8, 3, 9], [7, 6, 5, 11], [12, 13, 14, 15],]
+        history = []
+        expect_next_states = [
+            ([mv.DOWN], EPB([[1, 4, 10, 9], [2, 8, 3, None], [7, 6, 5, 11], [12, 13, 14, 15],])),
+            ([mv.LEFT], EPB([[1, 4, None, 10], [2, 8, 3, 9], [7, 6, 5, 11], [12, 13, 14, 15],])),
         ]
         next_states = EPB(s_init).next_valid_board_states(history)
         self.assertEqual(next_states, expect_next_states)
